3889d59b26c172b83aaccfa554c42466d887e2d1
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-05-30 14:21:25 +0530

    auth handler added

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import { Outlet } from "react-router-dom"
+import AuthHandler from "@/handler/auth-handler"
 
 const publicLayout = () => {
   return (
     <div className="w-full">
+        <AuthHandler/>
+
         <Header/>
 
         <Outlet/>
