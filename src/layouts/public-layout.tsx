a06a266742c0564db245e954a20e35f7c2b11a83
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-05-30 14:41:39 +0530

    home page added

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/header"
-import Footer from "@/components/footer"
+import {Footer} from "@/components/footer"
 import { Outlet } from "react-router-dom"
 import AuthHandler from "@/handler/auth-handler"
 
