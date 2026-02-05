77d373ad7b044564ca2ffafe654512a4a33eac66
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-05 19:37:58 +0530

    frontend url added for cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors";
-import aiRoutes from "./routes/ai.routes"
+import aiRoutes from "./routes/ai.routes";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-interview-app-sepia.vercel.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: allowedOrigins,
   credentials: true,
 }));
 
 app.use(express.json());
 
 app.use("/api/ai", aiRoutes);
-
 
 export default app;
